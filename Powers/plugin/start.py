021c766c4429f7fb120ae5bbd2f2279fb3e0606d
ChimkuOp <104584474+ChimkuOp@users.noreply.github.com>
2023-03-04 16:00:44 +0530

    Update start.py
    made changes in help menu

--- Powers/plugin/start.py
+++ Powers/plugin/start.py
@@ -23,31 +23,22 @@ async def start_(c: bot, m: Message):
 async def help_(c: bot, m: Message):
     txt = """
 Type /link in the chat to get invite link of the chat.
-Per join with you link will reward you some coin, which will further used to buy stuffs.
-
-Note that every /link will command will update the link you have created before in db
-and the user joined by that link will not considered as any base to give reward to you
+You get some coins for every user that join with your link, which will further used to buy stuffs.
 
 /mylink : To get previous genrated link of the chat by you.
-
 /profile <user id | username> : To get you information.
-
-/owners : Get info of owners
-
-/premium: To get link of premium channel. Works only in private chat
-
-/buy : To buy stuffs. Works only in private chat
+/myprofile : To check your coins
+/premium: To get link of premium channel. 
+/buy : To buy stuffs. 
 
 **OWNER ONLY**
 /addfile : To add file
 /rmfile <id of the file>: To remove file
 /addcat <name of CATEGORY should be str | pass nothing> : Add a new CATEGORY
+/owners : To get Owner's list
 /addowner <reply to user or his id> : Add user to owner list (Only be in list until the bot restarts)
 /rmowner <reply to user or his id> : Remove user to owner list (Only be in list until the bot restarts)
 
-**NOTE**: Your info will be stored in database when you type /link in the group
-
-**db** standas for **DataBase**
 """
     await m.reply_text(txt)
     return
